96fb7cd9d6f24dac54e14b5278102269ac5bb0ee
Saransh Malik <saransh.malik.cs.2019@miet.ac.in>
2023-09-22 18:58:39 +0530

    section added

--- js/extrenaljq.js
+++ js/extrenaljq.js
@@ -1,3 +1,60 @@
+// Selecting html elements
+
+const iconToggle = document.querySelector(".toggle_icon");
+const navbarMenu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu_link");
+const iconClose = document.querySelector(".close_icon");
+
+iconToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("active");
+});
+
+iconClose.addEventListener("click", () => {
+  navbarMenu.classList.remove("active");
+});
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", () => {
+    navbarMenu.classList.remove("active");
+  });
+});
+
+// change header backgroud
+function scrollHeader() {
+  const header = document.getElementById("header");
+  this.scrollY >= 20
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+}
+
+window.addEventListener("scroll", scrollHeader);
+
+//Scroll sections active links
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((section) => {
+//     const sectionHeight = section.offsetHeight;
+//     const sectionTop = section.offsetTop - 50;
+
+//     let sectionId = section.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".menu a[href *=" + sectionId + "]")
+//         .classList.add("active-link");
+//     } else {
+//       document
+//         .querySelector(".menu a[href *=" + sectionId + "]")
+//         .classList.remove("active-link");
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", scrollActive);
+
 //TYPE WRITING
 var TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
@@ -161,6 +218,9 @@ accordionHeaders.forEach((header) => {
 
 // swiper
 var swiper = new Swiper(".swiper", {
+  loop: true,
+  freeMode: true,
+
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
